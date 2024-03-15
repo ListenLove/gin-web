ec3d20093dcaab104d9b5667db9ce1b3b5abd700
陈也在哦 <wenbinchen150@icloud.com>
2024-03-15 12:11:32 +0800

    feat: 翻译器中间件存在多请求并发读写问题导致服务奔溃，现已修复

--- middleware/validator-translation.go
+++ middleware/validator-translation.go
@@ -1,48 +1,70 @@
 package middleware
 
 import (
-	"reflect"
-	"strings"
-
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
 	"github.com/go-playground/locales/en"
 	"github.com/go-playground/locales/zh"
-	"github.com/go-playground/locales/zh_Hant_TW"
 	ut "github.com/go-playground/universal-translator"
-	validator "github.com/go-playground/validator/v10"
-	en_translations "github.com/go-playground/validator/v10/translations/en"
-	zh_translations "github.com/go-playground/validator/v10/translations/zh"
+	"github.com/go-playground/validator/v10"
+	entranslations "github.com/go-playground/validator/v10/translations/en"
+	zhtranslations "github.com/go-playground/validator/v10/translations/zh"
+	"go.uber.org/zap"
+	"reflect"
+	"strings"
 )
 
-func Translations() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		// 准备多语言翻译器
-		uni := ut.New(en.New(), zh.New(), zh_Hant_TW.New())
-		locale := c.GetHeader("locale")
+var transMap = make(map[string]ut.Translator)
+var locales = []string{
+	"en",
+	"zh",
+}
+
+func init() {
+	// 准备多语言翻译器
+	uni := ut.New(en.New(), zh.New())
+	for _, locale := range locales {
 		trans, _ := uni.GetTranslator(locale)
-		v, ok := binding.Validator.Engine().(*validator.Validate)
+		vEngine, ok := binding.Validator.Engine().(*validator.Validate)
+		if !ok {
+			zap.L().Error("binding.Validator.Engine() 出错", zap.String("locale", locale))
+			return
+		}
 		// 注册一个函数，获取struct tag里自定义的json作为字段名
-		v.RegisterTagNameFunc(func(fld reflect.StructField) string {
+		vEngine.RegisterTagNameFunc(func(fld reflect.StructField) string {
 			name := strings.SplitN(fld.Tag.Get("json"), ",", 2)[0]
 			if name == "-" {
 				return ""
 			}
 			return name
 		})
-		// 注册翻译器
-		if ok {
-			switch locale {
-			case "zh":
-				_ = zh_translations.RegisterDefaultTranslations(v, trans)
-			case "en":
-				_ = en_translations.RegisterDefaultTranslations(v, trans)
-			default:
-				_ = zh_translations.RegisterDefaultTranslations(v, trans)
-			}
-			c.Set("trans", trans)
+		switch locale {
+		case "zh":
+			_ = zhtranslations.RegisterDefaultTranslations(vEngine, trans)
+			transMap[locale] = trans
+		case "en":
+			_ = entranslations.RegisterDefaultTranslations(vEngine, trans)
+			transMap[locale] = trans
+		default:
+			_ = zhtranslations.RegisterDefaultTranslations(vEngine, trans)
+			transMap[locale] = trans
 		}
+	}
+}
 
+func getTransByLocale(locale string) ut.Translator {
+	trans, ok := transMap[locale]
+	if !ok {
+		return transMap["zh"]
+	}
+	return trans
+}
+
+func Translations() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		locale := c.GetHeader("locale")
+		trans := getTransByLocale(locale)
+		c.Set("trans", trans)
 		c.Next()
 	}
 }
